b206a9207159e35748c750ac676eda711d427acf
María Báez <baezmarreromaria9@gamil.com>
2026-03-18 23:53:01 +0000

    Implemented content-based movie recommendation system v1 (no CLI)

---
+++ src/recommender.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+
+from sklearn.metrics.pairwise import cosine_similarity
+movies = pd.read_csv('data/processed_movies.csv', index_col=0)
+
+
+vectorizer = CountVectorizer(stop_words='english')
+vectors = vectorizer.fit_transform(movies['tags'])
+
+similarity = cosine_similarity(vectors)
+
+"""
+            Avatar   Titanic   Batman
+Avatar      1.0       0.1       0.8
+Titanic     0.1       1.0       0.2
+Batman      0.8       0.2       1.0
+"""
+def recommend(movie):
+    try:
+        movie_index = movies[movies['title'] == movie].index[0]
+    except IndexError:
+        print(f'Movie "{movie}" not found in the dataset.')
+        return
+
+    similarity_score = similarity[movie_index]
+
+    similarity_sorted = enumerate(similarity_score)
+    similarity_sorted = sorted(similarity_sorted, key=lambda x: x[1], reverse=True)
+    top_5_movies = similarity_sorted[1:6]
+
+    for i in top_5_movies:
+        index = i[0]
+        title = movies.iloc[index].title
+        print(f'title: {title}, similarity score: {similarity_score[index]*100:.2f}%')
+
+
+if __name__ == '__main__':
+    recommend('mariabaez')
+
+
